c564b7e2ac88f240e944820ac124cb0fa6568d8b
Riddhi Suteri <ridsuteri@gmail.com>
2022-08-15 23:55:55 +0530

    completed UI part of the application and integreated with connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:gsheets/gsheets.dart';
-import 'package:flutter_config/flutter_config.dart';
+import 'google_sheets_api.dart';
+import 'homepage.dart';
 
-// create  crediantials
-const _credentials = r'''
-{
-  "type": "service_account",
-  
-}
-
-''';
-
-// spreadsheet id
-
-const _spreadsheetId = '';
-void main() async {
-  // initiliaze the google sheets api
-  final gsheets = GSheets(_credentials);
-  final ss = await gsheets.spreadsheet(_spreadsheetId);
-  // get spreadsheet by it's title
-  var sheet = ss.worksheetByTitle('Worksheet1');
-
-  // updating a sheet
-  await sheet!.values.insertValue('Hello', column: 1, row: 1);
-  runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  GoogleSheetsApi().init();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -31,6 +13,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
